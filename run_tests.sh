49a6fb737eae3204b726e849d459e96318b561fc
guyvosco <guyvosco@gmail.com>
2025-12-21 09:26:57 +0200

    Refactor run_tests.sh to simplify package installation

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# Install the package (editable) with dev dependencies if you defined them
-if [ -f "pyproject.toml" ]; then
-  pip install -e ".[dev]"
-fi
+pip install -e ".[dev]"
 
-# Run the test suite
+pylint src/visualizeBGW
+
 pytest -q
